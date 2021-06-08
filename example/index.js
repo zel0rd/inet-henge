cfbe5c574c737806e73e8c5ea7e6217fd86911f9
younggil <zelord.kwoun@gmail.com>
2021-06-08 14:59:38 +0900

    [ADD] 탭기능 추가

---
+++ example/index.js
@@ -0,0 +1,18 @@
+console.log("TEST")
+let tabs = document.querySelectorAll(".aside__header")
+
+
+
+Array.from(tabs).map(function(tab){
+    tab.addEventListener('click', function(){
+        deactive()
+        this.classList.add("active")
+        console.log(this.id)
+        document.querySelectorAll(`#${this.id}`)[1].classList.add("tab_active")
+    })
+})
+
+function deactive() {
+    document.querySelector(".active").classList.remove("active")
+    document.querySelector(".tab_active").classList.remove("tab_active")
+}
